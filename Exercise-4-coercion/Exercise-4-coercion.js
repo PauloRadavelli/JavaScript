3f1d963e40cbacbd403914856a8529668c04da3d
paulosradavelli <paulosradavelli@gmail.com>
2022-07-19 10:57:13 -0300

    Type coercion exercise

--- Exercise-4-coercion/Exercise-4-coercion.js
+++ Exercise-4-coercion/Exercise-4-coercion.js
@@ -1,11 +1,22 @@
-var nameTrue = String("Paulo");
-var nameFalse = String("ab");
-function isValidName(name){
-    if(name.length == 0) {
+/*
+    Exercise 4: DEfina an "isValidName(...)" validator that
+    takes one parameter, "name". The validator returns "true" 
+    if all the following match the parameter ("false" otherwise);
 
+    -must be a string
+    -must be non-empty
+    -must contain non-whitespace of at least 3 characters
+*/
+var nameTrue = ("Paulo");
+var nameFalse = (123);
+function isValidName(name){
+    if (typeof name != 'string'){
         return false
     }
-    if (name.length < 3) {
+    else if(name.length == 0) {
+        return false
+    }
+    else if (name.replace(/\s/g, '').length != 0) {
         return false
     }
     else{
@@ -15,3 +26,32 @@ function isValidName(name){
 
  isValidName(nameTrue); // Should return true
 //isValidName(nameFalse); // Should return false
+
+//// exercise pt 2;
+function hoursAttended(attended, length){
+    attended = Number(attended);
+    length = Number(length);
+    if (typeof attended != 'string' && typeof attended != 'number'){
+        console.log(1)
+        return false
+    }
+    else if (typeof length != 'string' && typeof length != 'number'){
+        console.log(2)
+
+        return false
+    }
+    else if (attended < 0 || length < 0) {
+        console.log(3)
+
+        return false
+    }
+    else if (!Number.isInteger(attended) || !Number.isInteger(length)){
+        console.log(4)
+
+        return false
+    }
+    else return true
+}
+
+// hoursAttended("12.3", "12"); // should return false;
+hoursAttended("14","7"); // should return true;
